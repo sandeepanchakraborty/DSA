3778d846234c659b2883f258fb1bb36472f6cca3
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-02-22 11:59:21 +0530

    Time: 8 ms (27.11%) | Memory: 45 MB (36.26%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -18,29 +18,47 @@ class Solution {
     int n=0;
     public TreeNode recoverFromPreorder(String traversal) {
         n = traversal.length();
-        return recur(traversal,0);
+        return recur(traversal);
     }
-    public TreeNode recur(String traversal, int depth){
-        if(index >= n) return null;
-        //count the dash
-        int count=0;
-        int tempI=index;
-        while(tempI < n && !Character.isDigit(traversal.charAt(tempI))){
-            count++;
-            tempI++;
+    public TreeNode recur(String traversal){
+        Stack<TreeNode> stack = new Stack<>();
+        // if(index >= n) return null;
+        while(index < n){
+             //count the dash
+            int count=0;
+            while(index < n && !Character.isDigit(traversal.charAt(index))){
+                count++;
+                index++;
+            }
+            int depth = stack.size();
+            while(depth > count){
+                stack.pop();
+                depth--;
+            }
+
+            //find the number
+            int val=0;
+            while(index < n && Character.isDigit(traversal.charAt(index))){
+                val = val * 10 + (traversal.charAt(index) - '0');
+                index++;
+            }
+
+            TreeNode node = new TreeNode(val);
+            if(!stack.isEmpty()){
+                if(stack.peek().left == null){
+                    stack.peek().left = node;
+                }else{
+                    stack.peek().right = node;
+                }
+            }
+            stack.push(node);
+
         }
-        if(count!=depth) return null;
-        index = tempI;
-        //find the number
-        int val=0;
-        while(index < n && Character.isDigit(traversal.charAt(index))){
-            val = val * 10 + (traversal.charAt(index) - '0');
-            index++;
+       
+        while(stack.size()>1){
+            stack.pop();
         }
-        TreeNode node = new TreeNode(val);
-        node.left = recur(traversal, depth+1);
-        node.right = recur(traversal, depth+1);
-        return node;
+        return stack.peek();
     }
 
 }
