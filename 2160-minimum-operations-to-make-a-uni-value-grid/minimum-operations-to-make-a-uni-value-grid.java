60f349edc856c808086e57de8ebb8fef9ed14ac5
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-03-26 21:29:12 +0530

    Time: 3 ms (100.00%) | Memory: 63.9 MB (99.50%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,22 +1,46 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        List<Integer> arr = new ArrayList<>();
-        for (int[] row : grid) {
-            for (int num : row) {
-                arr.add(num);
+
+        int m = grid.length, n = grid[0].length;
+
+        int len = m * n;
+        if(len < 2) return 0;
+        
+        int[] count = new int[10001];
+
+        int totalSum = 0;
+        int r = grid[0][0] % x;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                int v = grid[i][j];
+                if(v % x != r) return -1;
+                count[v]++;
+            } 
+        }
+        
+        len = (len + 1) / 2;
+        int total = 0;
+        int median = 0;
+
+        for(int v = 0; v < count.length; ++v){
+            if(count[v] == 0) continue;
+            total += count[v];
+            if(total >= len){
+                median = v;
+                break;
             }
         }
         
-        Collections.sort(arr);
-        int median = arr.get(arr.size() / 2);
-        int operations = 0;
-
-        for (int num : arr) {
-            int diff = Math.abs(num - median);
-            if (diff % x !=0 ) return -1;
-            operations += diff / x;
+        int ans = 0;
+        for(int v = 0; v <= median; ++v){
+            if(count[v] == 0) continue;
+            ans += count[v] * (median - v) / x;
         }
-        return operations;
-    }
 
+        for(int v = median + 1; v < count.length; ++v){
+            if(count[v] == 0) continue;
+            ans += count[v] * (v - median) / x;
+        }        
+        return ans;
+    }
 }
