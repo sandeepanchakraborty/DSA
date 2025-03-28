1f4240395fccda7c1d6d3c41564aac00abbc4465
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-03-28 20:42:50 +0530

    Time: 159 ms (25.19%) | Memory: 56.2 MB (87.02%) - LeetSync

---
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] maxPoints(int[][] grid, int[] queries) {
+        int rows = grid.length, cols = grid[0].length, qLen = queries.length;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        int[][] qIndex = new int[qLen][2];
+        for (int i = 0; i <qLen; i++) {
+            qIndex[i] = new int[]{queries[i], i};
+        }
+        Arrays.sort(qIndex, Comparator.comparingInt(a -> a[0]));
+ 
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{grid[0][0], 0, 0});
+        
+        boolean[][] visited = new boolean[rows][cols];
+        visited[0][0] = true;
+        
+        int[] result = new int[qLen];
+        int count = 0;
+        
+        for (int[] q : qIndex) {
+            int query = q[0], index = q[1];
+
+            while (!pq.isEmpty() && pq.peek()[0] < query) {
+                int[] cell = pq.poll();
+                int r = cell[1], c = cell[2];
+                count++;
+
+                for (int[] dir : directions) {
+                    int nr = r + dir[0], nc = c + dir[1];
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[nr][nc]) {
+                        pq.offer(new int[]{grid[nr][nc], nr, nc});
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+            result[index] = count;
+        }
+        
+        return result;
+    }
+}
