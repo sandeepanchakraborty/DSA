dd546416b12d298ec678ab151c759425eb013934
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-04-04 14:50:13 +0530

    Time: 3 ms (70.50%) | Memory: 48.7 MB (6.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
-        List<Integer> ans=new ArrayList<>();
+        int count1=0,count2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
+
         for(int i=0;i<n;i++){
-            if(ans.size()==0 || ans.get(0)!=nums[i]){
-              int count=0;
-                for(int j=0;j<n;j++){
-                    if(nums[j]==nums[i]){
-                        count++;
-                    }
-                }
-                    if(count>n/3){
-                        ans.add(nums[i]);
-                    }
-                      if(ans.size()==2) break; 
-                } 
+            if(count1==0 && nums[i]!=ele2){
+                count1=1;
+                ele1=nums[i];
+            }else if(count2==0 && nums[i]!=ele1){
+                 count2=1;
+                ele2=nums[i];
+            }else if(ele1==nums[i]){ count1++;}
+            else if(ele2==nums[i]) {count2++;}
+            else{
+                count1--;count2--;
+            }
         }
+
+        List<Integer> ans=new ArrayList<>();
+        count1=0;count2=0;
+        for(int i=0;i<n;i++ ){
+            if(nums[i]==ele1)count1++;
+            if(nums[i]==ele2)count2++;
+        }
+        int res=n/3;
+        if(count1>res) ans.add(ele1);
+        if(count2>res) ans.add(ele2);
+
+
         return ans;
     }
+
 }
