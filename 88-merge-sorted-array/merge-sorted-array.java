659d981e35afe21852de488919efc1526bdc87b1
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-04-10 12:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (60.72%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,19 +1,36 @@
-public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+class Solution {
+    public void merge(int[] nums1, int n, int[] nums2, int m) {
+        // Create a new array to store the merged results
+        long nums3[] = new long[n + m];
         
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        int right = 0;  // Pointer for nums2
+        int left = 0;   // Pointer for nums1
+        int index = 0;  // Pointer for nums3
+
+        // Merge both arrays while there are elements in nums1 and nums2
+        while (left < n && right < m) {
+            if (nums1[left] < nums2[right]) {
+                nums3[index] = nums1[left];
+                index++;
+                left++;
             } else {
-                nums1[k--] = nums2[j--];
+                nums3[index] = nums2[right];
+                index++;
+                right++;
             }
         }
-       
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+
+        // Add remaining elements from nums1 (if any)
+        while (left < n) {
+            nums3[index++] = nums1[left++];
         }
+
+        // Add remaining elements from nums2 (if any)
+        while (right < m) {
+            nums3[index++] = nums2[right++];
+        }
+        for (int i = 0; i < nums3.length; i++) {
+            nums1[i] = (int) nums3[i];
     }
+}
 }
