3ea76f3747a1125dabb57569dd69227f43873022
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-04-29 00:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (62.52%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,15 +1,12 @@
 class Solution {
     public void reverseString(char[] s) {
-        StringBuilder ans=new StringBuilder(new String(s));
-        int n=s.length;
-        for(int i=0;i<n/2;i++){
-            char FirstChar=ans.charAt(i);
-            char LastChar=ans.charAt(n-1-i);
-            ans.setCharAt(i,LastChar);
-            ans.setCharAt(n-1-i,FirstChar);
-        }
-        for(int i=0;i<n;i++){
-            s[i]=ans.charAt(i);
-        }
+       int n=s.length;
+       for(int i=0;i<n/2;i++){
+        char FirstChar=s[i];
+        char LastChar=s[n-1-i];
+        s[i]=LastChar;
+        s[n-1-i]=FirstChar;
+
+       } 
     }
 }
