0e4d540c385fdd89d323ea526af8278e813f1c1a
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-06-02 00:02:50 +0530

    Time: 2 ms (82.14%) | Memory: 45.9 MB (65.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,23 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
-        int n = arr.length;
-        int j = -1;
-
-        // Find the first occurrence of zero
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                j = i;
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
                 break;
             }
         }
 
-        // If there are no zeros, return
-        if (j == -1) return;
+            if(j==-1) return ;
 
-        // Move non-zero elements to the left and fill zeros at the end
-        for (int i = j + 1; i < n; i++) {
-            if (arr[i] != 0) {
-                arr[j] = arr[i];
-                arr[i] = 0;
-                j++;
+            for(int i=j+1;i<n;i++){
+                if(nums[i]!=0){
+                    nums[j]=nums[i];
+                    nums[i]=0;
+                    j++;
+                }
             }
-        }
+        
     }
 }
