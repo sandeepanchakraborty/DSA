6282d30ae529021ce8306ba80ce65ed94515fb1f
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-06-01 23:49:33 +0530

    Time: 1 ms (61.37%) | Memory: 44.8 MB (60.29%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int ans=1;
         for(int i=1;i<n;i++){
-            if (nums[i] != nums[i - 1]){
+            if(nums[i]!=nums[i-1]){
                 nums[ans]=nums[i];
                 ans++;
             }
