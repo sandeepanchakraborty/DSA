eda1b70bfdf2692fbc0e10f2e819fd35ab43e49b
sandeepanchakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-07-01 00:35:10 +0530

    Time: 10 ms (45.58%) | Memory: 56.8 MB (55.89%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,21 +10,59 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-          List<Integer> arr=new ArrayList<>();
+        if(head==null || head.next==null){
+            return head;
+        }
 
-        ListNode temp=head;
+        ListNode mid=findmiddle( head);
 
-        while(temp!=null){
-            arr.add(temp.val);
+        ListNode right=mid.next;
+        mid.next=null;
+        ListNode left=head;
+
+        left=sortList(left);
+        right=sortList(right);
+        return mergeTwoSortedLinkedLists(left,right);
+    }
+
+    public ListNode findmiddle(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode mergeTwoSortedLinkedLists(ListNode list1,ListNode list2){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }else{
+                temp.next=list2;
+                list2=list2.next;
+            }
             temp=temp.next;
         }
-        Collections.sort(arr);
-
-        temp=head;
-        for(int val:arr){
-            temp.val=val;
-            temp=temp.next;
+        if(list1!=null){
+            temp.next=list1;
+            list1=list1.next;
         }
-        return head;
+        
+        if(list2!=null){
+            temp.next=list2;
+            list2=list2.next;
+        }
+        return dummy.next;
+
     }
 }
