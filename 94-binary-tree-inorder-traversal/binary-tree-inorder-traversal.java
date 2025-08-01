0ab752d274786a3413446acf0691c76f015d2d26
Sandeepan Chakraborty <125343806+sandeepanchakraborty@users.noreply.github.com>
2025-08-01 15:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (44.16%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+         List<Integer> arr=new ArrayList<>();
+        func(root,arr);
+        return arr;
+    }
+    public void func(TreeNode root,List<Integer> ans){
+        if(root==null){
+            return;
+
+        }
+        func(root.left,ans);
+        ans.add(root.val);
+        func(root.right,ans);
+    }
+}
